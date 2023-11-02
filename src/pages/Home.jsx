a4423fae74d77e1b0a04345d82294f5fa8da41bd
Andrej Bozin <82775190+AndrejBozin@users.noreply.github.com>
2023-11-02 14:40:57 -0400

    Announcement Component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import Nav from '../components/Nav'
+import Announcement from '../components/Announcement'
 const Home = () => {
   return (
     <div>
+      <Announcement/>
       <Nav/>
     </div>
   )
