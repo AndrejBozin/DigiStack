113d1a9798c7a947d1da2980261f596ee7c3bddf
Andrej Bozin <82775190+AndrejBozin@users.noreply.github.com>
2023-11-02 15:12:32 -0400

    Slider Container

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Nav from '../components/Nav'
 import Announcement from '../components/Announcement'
+import Slider from '../components/Slider'
 const Home = () => {
   return (
     <div>
       <Announcement/>
       <Nav/>
+      <Slider/>
     </div>
   )
 }
