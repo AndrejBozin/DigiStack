b212c5265f343ae42004b9ca628d9b829e7167a6
Andrej Bozin <82775190+AndrejBozin@users.noreply.github.com>
2023-11-03 13:55:04 -0400

    Added Initial Supplement Data

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://freerangestock.com/sample/137854/white-background-with-scattered-pills.jpg",
+      title: "FALL SALE",
+      desc: "DON'T COMPROMISE ON HEALTH! GET FLAT 30% OFF FOR NEW SUPPLEMENTS.",
+      bg: "f5fafd",
+    },
+    {
+      id: 2,
+      img: "https://i.ibb.co/DG69bQ4/2.png",
+      title: "WINTER SALE",
+      desc: "DON'T COMPROMISE ON HEALTH! GET FLAT 30% OFF FOR NEW SUPPLEMENTS.",
+      bg: "fcf1ed",
+    },
+    {
+      id: 3,
+      img: "https://i.ibb.co/cXFnLLV/3.png",
+      title: "SPRING SALE",
+      desc: "DON'T COMPROMISE ON HEALTH! GET FLAT 30% OFF FOR NEW SUPPLEMENTS.",
+      bg: "fbf0f4",
+    },
+  ];
