c8f3a75fabfdd903ae01fce8f8b2f3136f14dd3a
Andrej Bozin <82775190+AndrejBozin@users.noreply.github.com>
2023-11-07 13:59:47 -0500

    Implemented categoires container

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Nav from '../components/Nav'
 import Announcement from '../components/Announcement'
 import Slider from '../components/Slider'
+import Categories from '../components/Categories'
 const Home = () => {
   return (
     <div>
       <Announcement/>
       <Nav/>
       <Slider/>
+      <Categories/>
     </div>
   )
 }
